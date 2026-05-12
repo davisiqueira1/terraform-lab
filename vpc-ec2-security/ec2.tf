7ffb15d674a2ad5f30aa34c6cbb36941313576ed
Davi Siqueira <davisvsiqueira@gmail.com>
2026-05-11 22:26:12 -0300

    feat(vpc-ec2-security): add ec2 instances

---
+++ vpc-ec2-security/ec2.tf
@@ -0,0 +1,26 @@
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical ID
+}
+
+resource "aws_instance" "private_instance" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+}
+
+resource "aws_instance" "private_instance" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+}
