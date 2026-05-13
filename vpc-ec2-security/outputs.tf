c58e121013030ce1b95bee0f4a8abd9a17413ec1
Davi Siqueira <davisvsiqueira@gmail.com>
2026-05-13 19:58:01 -0300

    feat(vpc-ec2-security): output public and private EC2 IP addresses

--- vpc-ec2-security/outputs.tf
+++ vpc-ec2-security/outputs.tf
@@ -1,3 +1,11 @@
 output "vpc_arn" {
   value = aws_vpc.main.arn
 }
+
+output "public_instance_public_ip_address" {
+  value = aws_instance.public_instance.public_ip
+}
+
+output "private_instance_private_ip_address" {
+  value = aws_instance.private_instance.private_ip
+}
