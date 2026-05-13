6857320262682d370ec061fcfc9f8d11ad9576e2
Davi Siqueira <davisvsiqueira@gmail.com>
2026-05-12 21:05:45 -0300

    style(vpc-ec2-security): run formatter on variables file

--- vpc-ec2-security/variables.tf
+++ vpc-ec2-security/variables.tf
@@ -7,55 +7,55 @@ variable "aws_region" {
 
 variable "vpc_cidr_block" {
   description = "CIDR range used by the VPC"
-  default  = "10.0.0.0/16"
-  type     = string
-  nullable = true
+  default     = "10.0.0.0/16"
+  type        = string
+  nullable    = true
 }
 
 variable "public_subnet_cidr_block" {
   description = "CIDR range for the public subnet"
-  default  = "10.0.1.0/24"
-  type     = string
-  nullable = true
+  default     = "10.0.1.0/24"
+  type        = string
+  nullable    = true
 }
 
 variable "private_subnet_cidr_block" {
   description = "CIDR range for the private subnet"
-  default  = "10.0.2.0/24"
-  type     = string
-  nullable = true
+  default     = "10.0.2.0/24"
+  type        = string
+  nullable    = true
 }
 
 variable "public_subnet_az" {
   description = "Availability zone for the public subnet"
-  default  = "us-east-1a"
-  type     = string
-  nullable = false
+  default     = "us-east-1a"
+  type        = string
+  nullable    = false
 }
 
 variable "private_subnet_az" {
   description = "Availability zone for the private subnet"
-  default  = "us-east-1a"
-  type     = string
-  nullable = false
+  default     = "us-east-1a"
+  type        = string
+  nullable    = false
 }
 
 variable "trusted_ssh_cidr_block" {
   description = "Trusted IPv4 CIDR block allowed to access instances via SSH"
-  type = string
-  nullable = false
+  type        = string
+  nullable    = false
 }
 
 variable "public_instance_ip_address" {
   description = "Private IPv4 address assigned to the EC2 instance in the public subnet"
-  default = "10.0.1.10"
-  type = string
-  nullable = false
+  default     = "10.0.1.10"
+  type        = string
+  nullable    = false
 }
 
 variable "private_instance_ip_address" {
   description = "Private IPv4 address assigned to the EC2 instance in the private subnet"
-  default = "10.0.2.10"
-  type = string
-  nullable = false
+  default     = "10.0.2.10"
+  type        = string
+  nullable    = false
 }
