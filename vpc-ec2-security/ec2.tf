820e3e9d9cc6b32eae201133b39e5f17fd79fe9c
Davi Siqueira <davisvsiqueira@gmail.com>
2026-05-13 20:00:07 -0300

    style(vpc-ec2-security): run formatter on ec2 file

--- vpc-ec2-security/ec2.tf
+++ vpc-ec2-security/ec2.tf
@@ -18,8 +18,8 @@ resource "aws_instance" "public_instance" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t3.micro"
 
-  subnet_id = aws_subnet.public_subnet.id
-  private_ip = var.public_instance_ip_address
+  subnet_id              = aws_subnet.public_subnet.id
+  private_ip             = var.public_instance_ip_address
   vpc_security_group_ids = [aws_security_group.public_instance.id]
 
   associate_public_ip_address = true
@@ -31,14 +31,14 @@ resource "aws_instance" "private_instance" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t3.micro"
 
-  subnet_id = aws_subnet.private_subnet.id
-  private_ip = var.private_instance_ip_address
+  subnet_id              = aws_subnet.private_subnet.id
+  private_ip             = var.private_instance_ip_address
   vpc_security_group_ids = [aws_security_group.private_instance.id]
 
   key_name = aws_key_pair.ssh_key_pair.key_name
 }
 
 resource "aws_key_pair" "ssh_key_pair" {
-  key_name = "ssh-key"
+  key_name   = "ssh-key"
   public_key = file(var.ssh_public_key_path)
 }
